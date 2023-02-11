3fa7fd37a9e7d589af0034dd5c5874b82fe7a7ad
Alexey Smirnov <Sibusky@yandex.ru>
2023-02-11 17:56:29 +0400

    feat: add <PageNotFound />

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './PageNotFound.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function PageNotFound() {
+  let navigate = useNavigate();
+  const goBack = () => navigate(-1); // Возврат назад
+
   return (
-    <main>
-        <h1>PageNotFound</h1>
-        
+    <main className='page-not-found'>
+      <h2 className='page-not-found__title'>404</h2>
+      <p className='page-not-found__text'>Page not found</p>
+      <p className='page-not-found__link-back link' onClick={goBack}>
+        Go back
+      </p>
     </main>
-  )
+  );
 }
